cd8baca38e6b8aedeb1152d68a5708779e850080
AllenPianoMan <allenn_farcas@yahoo.com>
2018-06-01 15:13:46 +0300

    Added the project

---
+++ LFTC-project/cmake-build-debug/CMakeFiles/LFTC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LFTC.dir/main.c.o"
+  "LFTC.pdb"
+  "LFTC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LFTC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
